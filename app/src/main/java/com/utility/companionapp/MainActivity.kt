5a49b8f5088b50039cee92ac707894607acc7f9c
Sanika Vanjari <sanikavanjari94@gmail.com>
2020-12-27 13:55:40 +0530

    validation added for year field with UI

--- app/src/main/java/com/utility/companionapp/MainActivity.kt
+++ app/src/main/java/com/utility/companionapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.utility.companionapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.utility.companionapp.databinding.ActivityMainBinding
 
@@ -11,9 +12,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding= DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        binding.text.text = "Sanika"
+        binding.leapYearButton.setOnClickListener {
+            if (!binding.yearField.text.isNullOrEmpty()) {
+                binding.resultText.text = setResultText()
+            } else {
+                Toast.makeText(this, "Year field cannot be empty..", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
 
+    private fun setResultText(): String {
+        return "COMMON YEAR"
     }
 }
